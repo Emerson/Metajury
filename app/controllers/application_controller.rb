eb29ad8e1cfd92872c558456f3d094940f28b8e5
Emerson Lackey <emerson@plankdesign.com>
2011-11-17 13:40:17 -0500

    added a menu to toggle between the admin view and the standard user view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,6 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
-
   def require_admin
     self.current_user
     if controller_path.match(/admin/i)
