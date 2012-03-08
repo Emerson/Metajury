f8622edd8d6d272581b31b54497462e9ee530108
Emerson Lackey <emerson@plankdesign.com>
2012-03-08 13:55:22 -0500

    adding travis script

---
+++ ci/travis.rb
@@ -0,0 +1,5 @@
+# Copy Application Settings
+FileUtils.cp File.expand_path('../config/application_settings.example.yml') File.expand_path('../config/application_settings.yml')
+
+# Copy Email Settings
+FileUtils.cp File.expand_path('../config/email.example.yml') File.expand_path('../config/email.yml')
