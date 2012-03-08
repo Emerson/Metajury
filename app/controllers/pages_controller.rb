f2838b298e8ba8f7b695c0d7bc682541d412aa0c
Emerson Lackey <emerson.lackey@gmail.com>
2012-03-07 23:17:46 -0500

    now using score instead of created_by

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,10 @@ class PagesController < ApplicationController
 
  def home
   if @current_user
-    @submissions = Submission.order('created_at desc').limit(50).all
+    @submissions = Submission.order('score desc').limit(50).all
     render 'user_homepage'    
   else
-    @submissions = Submission.order('created_at desc').limit(50).all
+    @submissions = Submission.order('score desc').limit(50).all
     render 'homepage'
   end
  end
