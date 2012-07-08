634a0881545adc09b22398b669a7478c90b422f3
Emerson Lackey <emerson.lackey@gmail.com>
2012-07-08 18:58:22 -0400

    precompiling assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,6 +67,17 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+       logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+
 
   task :apply_configs, :roles => :app do
     run "cp /home/#{user}/#{application}/shared/configs/database.yml #{release_path}/config/database.yml"
@@ -84,6 +95,7 @@ namespace :deploy do
 
 end
 
+before "deploy:assets:precompile", "bundle:install"
 before 'deploy:setup', 'rvm:install_ruby'
 after 'deploy:update_code', 'deploy:apply_configs'
 after 'deploy:update_code', 'deploy:create_branch_file'
