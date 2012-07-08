fd8214ece65bba30ce1e3133ebd5c03292b986e6
Emerson Lackey <emerson.lackey@gmail.com>
2012-07-08 18:32:53 -0400

    small unicorn tweak

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -16,16 +16,16 @@ stderr_path APP_PATH + "/log/unicorn.stderr.log"
 stdout_path APP_PATH + "/log/unicorn.stdout.log"
 
 # Preload Setup
-preload_app true
-GC.respond_to?(:copy_on_write_friendly=) and
-  GC.copy_on_write_friendly = true
+# preload_app true
+# GC.respond_to?(:copy_on_write_friendly=) and
+#   GC.copy_on_write_friendly = true
 
-before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
-end
+# before_fork do |server, worker|
+#   defined?(ActiveRecord::Base) and
+#     ActiveRecord::Base.connection.disconnect!
+# end
 
-after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
-end
+# after_fork do |server, worker|
+#   defined?(ActiveRecord::Base) and
+#     ActiveRecord::Base.establish_connection
+# end
