e4559800b656987a93632d0a8b49a7ed40adf67a
Emerson Lackey <emerson.lackey@gmail.com>
2012-07-08 17:45:31 -0400

    more updates to unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,9 +7,11 @@ rails_env = ENV['RAILS_ENV'] || 'production'
 worker_processes 4
 timeout 30
 
-# Socket and Pid Location
+# Socket, PID and Log Location
 pid working_dir + '/tmp/pids/unicorn.pid'
 listen working_dir '/tmp/sockets/unicorn.sock', :backlog => 64
+stderr_path APP_PATH + "/log/unicorn.stderr.log"
+stdout_path APP_PATH + "/log/unicorn.stdout.log"
 
 # Preload Setup
 preload_app true
